c83bc0339566737742f3475b31f4764a6ff15040
Nicola Venuti <venuti@amazon.com>
2024-04-26 07:28:54 +0000

    get ready for when the repo is public

--- ParallelCluster/post-install/post-install.headnode.sh
+++ ParallelCluster/post-install/post-install.headnode.sh
@@ -2,3 +2,5 @@
 
 curl -sL -O https://raw.githubusercontent.com/spack/spack-configs/main/AWS/parallelcluster/postinstall.sh
 sudo bash postinstall.sh
+
+#git clone https://github.com/aws-samples/hpc-applications.git
