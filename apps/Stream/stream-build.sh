55f88a2dc165be785edcf1047a5b968f36dfb511
Nicola Venuti <venuti@amazon.com>
2026-04-02 13:29:33 -0700

    perf(stream): Optimize STREAM benchmark for Hpc8a (EPYC 5th Gen)
    Upgrade AOCC compiler from 4.0.0 to 5.0.0 for znver5 support.
    Use -march=znver5 instead of -march=native to explicitly target
    Turin architecture. Comment out Hpc6a config so only Hpc8a
    settings are active by default.

--- apps/Stream/stream-build.sh
+++ apps/Stream/stream-build.sh
@@ -4,13 +4,13 @@ if test -f "stream.c"; then
     rm stream.c
 fi
 
-wget https://download.amd.com/developer/eula/aocc-compiler/aocc-compiler-4.0.0.tar
-tar -xf aocc-compiler-4.0.0.tar
-cd aocc-compiler-4.0.0
+wget https://download.amd.com/developer/eula/aocc-compiler/aocc-compiler-5.0.0.tar
+tar -xf aocc-compiler-5.0.0.tar
+cd aocc-compiler-5.0.0
 ./install.sh
 cd ../
 source ./setenv_AOCC.sh
 
 wget https://raw.githubusercontent.com/jeffhammond/STREAM/master/stream.c
 
-clang stream.c -fopenmp -mcmodel=large -DSTREAM_TYPE=double -DSTREAM_ARRAY_SIZE=560000000 -DNTIMES=100 -ffp-contract=fast -fnt-store -O3 -Ofast -ffast-math -ffinite-loops -march=native -zopt -fremap-arrays -mllvm -enable-strided-vectorization -fvector-transform  -o stream
+clang stream.c -fopenmp -mcmodel=large -DSTREAM_TYPE=double -DSTREAM_ARRAY_SIZE=560000000 -DNTIMES=100 -ffp-contract=fast -fnt-store -O3 -Ofast -ffast-math -ffinite-loops -march=znver5 -zopt -fremap-arrays -mllvm -enable-strided-vectorization -fvector-transform  -o stream
