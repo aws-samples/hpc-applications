1fdbe7f008f09812e6f2d6ec72c5e34f5f77ae97
Nicola Venuti <venuti@amazon.com>
2026-04-02 13:41:14 -0700

    fix(stream): Switch AOCC install from tar to RPM package
    AMD removed the tar download for AOCC 5.0. Install via RPM instead
    using yum localinstall. Update setenv_AOCC.sh path to match the
    standard RPM install location at /opt/AMD/aocc-compiler-5.0.0/.

--- apps/Stream/stream-build.sh
+++ apps/Stream/stream-build.sh
@@ -1,16 +1,40 @@
-#! /bin/bash
+#!/bin/bash
+set -e
 
+# Install AOCC 5.0 compiler (AMD Optimizing C/C++ Compiler)
+if ! command -v /opt/AMD/aocc-compiler-5.0.0/bin/clang &> /dev/null; then
+    echo "==> Installing AOCC 5.0.0..."
+    wget -q https://download.amd.com/developer/eula/aocc-compiler/aocc-compiler-5.0.0-1.x86_64.rpm
+    sudo yum localinstall -y aocc-compiler-5.0.0-1.x86_64.rpm
+    rm -f aocc-compiler-5.0.0-1.x86_64.rpm
+fi
+
+# Source AOCC environment
+source /opt/AMD/aocc-compiler-5.0.0/setenv_AOCC.sh
+
+# Download STREAM source
 if test -f "stream.c"; then
     rm stream.c
 fi
+wget -q https://raw.githubusercontent.com/jeffhammond/STREAM/master/stream.c
 
-wget https://download.amd.com/developer/eula/aocc-compiler/aocc-compiler-5.0.0.tar
-tar -xf aocc-compiler-5.0.0.tar
-cd aocc-compiler-5.0.0
-./install.sh
-cd ../
-source ./setenv_AOCC.sh
+# Compile with AOCC-specific optimizations for AMD EPYC 5th Gen (znver5)
+echo "==> Compiling STREAM..."
+clang stream.c \
+    -fopenmp \
+    -mcmodel=large \
+    -DSTREAM_TYPE=double \
+    -DSTREAM_ARRAY_SIZE=560000000 \
+    -DNTIMES=100 \
+    -ffp-contract=fast \
+    -fnt-store \
+    -O3 -Ofast -ffast-math \
+    -ffinite-loops \
+    -march=znver5 \
+    -zopt \
+    -fremap-arrays \
+    -mllvm -enable-strided-vectorization \
+    -fvector-transform \
+    -o stream
 
-wget https://raw.githubusercontent.com/jeffhammond/STREAM/master/stream.c
-
-clang stream.c -fopenmp -mcmodel=large -DSTREAM_TYPE=double -DSTREAM_ARRAY_SIZE=560000000 -DNTIMES=100 -ffp-contract=fast -fnt-store -O3 -Ofast -ffast-math -ffinite-loops -march=znver5 -zopt -fremap-arrays -mllvm -enable-strided-vectorization -fvector-transform  -o stream
+echo "==> Done. Run with: sbatch stream.sbatch"
